9c4f30b10205604c1e9da71bffe7db46b9ad3153
jiro.aqua@gmail.com <jiro.aqua@gmail.com@1a681ed7-1c64-c7c6-6d5f-b5ed6cd9b0bb>
2012-06-09 05:54:21 +0000

    [2012/06/09 v.0.2.18] Workaround for ICS updated Xperia. Added syntax file for TeX. (Thanks to MaQiYuan!)

--- trunk/src/jp/sblo/pandora/jota/text/SpannableStringBuilder.java
+++ trunk/src/jp/sblo/pandora/jota/text/SpannableStringBuilder.java
@@ -1059,17 +1059,17 @@ implements CharSequence, GetChars, Spannable, Editable, Appendable,
                          float x, float y, Paint p) {
         checkRange("drawText", start, end);
 
-        if (end <= mGapStart) {
-            c.drawText(mText, start, end - start, x, y, p);
-        } else if (start >= mGapStart) {
-            c.drawText(mText, start + mGapLength, end - start, x, y, p);
-        } else {
+//        if (end <= mGapStart) {
+//            c.drawText(mText, start, end - start, x, y, p);
+//        } else if (start >= mGapStart) {
+//            c.drawText(mText, start + mGapLength, end - start, x, y, p);
+//        } else {
             char[] buf = TextUtils.obtain(end - start);
 
             getChars(start, end, buf, 0);
             c.drawText(buf, 0, end - start, x, y, p);
             TextUtils.recycle(buf);
-        }
+//        }
     }
 
     /**
